e835fb289830b86376cafef0e24909d8531c8fc5
Josie <tinaweizq@outlook.com>
2024-08-11 22:26:15 -0700

    refactor: Update postCount.json and remove unused countPosts.js file

--- src/utils/countPosts.js
+++
@@ -1,7 +0,0 @@
-import glob from "glob";
-import path from "path";
-
-export function countPosts() {
-  const postsDir = path.join(process.cwd(), "src/pages/posts/*.{md,mdx}");
-  return glob.sync(postsDir).length;
-}
